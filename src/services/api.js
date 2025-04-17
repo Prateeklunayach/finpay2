8816fd5b635c5696b503a0db6263647d69421d27
Prateek lunayach <lunayachprateek@gmail.com>
2025-04-18 03:27:38 +0530

    dnd

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://da18cb3d-90d8-4e21-b63e-19d719f06fcf-00-321o5acwph97q.sisko.replit.dev:3001/';
+const API_BASE_URL = 'https://da18cb3d-90d8-4e21-b63e-19d719f06fcf-00-321o5acwph97q.sisko.replit.dev:3001';
 
 export const registerUser = async (userData) => {
   try {
